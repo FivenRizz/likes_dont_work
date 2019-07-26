5aefe86731e698278f0fc973f9ab7a4d76a0f9d5
Ashoka Shringla <ashokashringla@gmail.com>
2019-07-26 14:56:48 -0400

    Add files via upload

---
+++ likes_dont_work/src/app/post-service.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+
+import { Post } from './post/Post';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { componentHostSyntheticProperty } from '@angular/core/src/render3';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostServiceService {
+
+  constructor(private _httpService: HttpClient) { }
+  getPosts(): Observable<any>{
+   return this._httpService.get("http://localhost:8080/Rev_SpringMVC_Hello/api/posts")
+  }
+
+  addPost(post: Post){
+    let body = JSON.parse(JSON.stringify(post));
+    
+    if(post.post_id){
+      console.log("Entered for updates");
+      return this._httpService.post("http://localhost:8080/Rev_SpringMVC_Hello/api/posts", body);
+
+    }else{
+    return this._httpService.post("http://localhost:8080/Rev_SpringMVC_Hello/api/posts",body);
+    }
+  }
+
+  deletePost(postId: number){
+    return this._httpService.delete("http://localhost:8080/Rev_SpringMVC_Hello/api/posts/"+postId);
+  }
+
+  getPostByUserID(userId: number): Observable<any>{
+    return this._httpService.get("http://localhost:8080/Rev_SpringMVC_Hello/api/posts/"+userId);
+  }
+
+  getPostsNotUserID(userId: number): Observable<any>{
+    return this._httpService.get("http://localhost:8080/Rev_SpringMVC_Hello/api/posts/not/"+userId);
+  }
+
+  likePost(postId: number){
+    console.log("in like" + postId)
+    return this._httpService.get("http://localhost:8080/Rev_SpringMVC_Hello/api/posts/mylike/"+postId);
+  }
+
+  sendLike(idnumber: number){
+    console.log("likes");
+    this._httpService.get("http://localhost:8080/Rev_SpringMVC_Hello/api/posts/mylike/"+idnumber);
+  }
+
+}
+
---
+++ likes_dont_work/src/app/postlist/postlist.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+// import
+import { Post } from '../post/Post';
+import { PostServiceService } from '../post-service.service';
+import {} from '../login/login.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-postlist',
+  templateUrl: './postlist.component.html',
+  styleUrls: ['./postlist.component.css']
+})
+export class PostlistComponent implements OnInit {
+  posts:Post[];
+  statusMessage: string
+  post = new Post();
+
+id:number;
+
+  constructor(private router: Router, private _postService:PostServiceService) { }
+  ngOnInit() {
+    this.id = +localStorage.getItem('user_id');
+    //this.id = 1;
+    this.getPosts();
+  }
+
+  getPosts():void
+  {
+    if(this.id != null && this.id != 0){
+
+    this._postService.getPostsNotUserID(this.id)
+    .subscribe((postData) =>
+                                {this.posts = postData;
+                                console.log(this.id);},
+                                (error) => {console.log(error);
+                                this.statusMessage = "Problem with service"
+                                }
+    );
+                              }
+                              else{
+                                this.posts = null;
+                              }
+  }
+
+  likePost(id: number):void
+  {
+    console.log(id);
+    this._postService.sendLike(id);
+  }
+
+  goHome():void{
+    this.router.navigate(['/Home']);
+}
+
+}
